ecef5bc157a54d52b68435400006060324032f7c
shani <d4developer01@gmail.com>
2024-05-23 21:01:39 +0500

    first commit

---
+++ app/src/main/java/com/app/localgottalent1/utils/Config.java
@@ -0,0 +1,12 @@
+package com.app.localgottalent1.utils;
+
+public class Config {
+
+    public static String ADMOB_SMALL_BANNER_AD_ID = "ca-app-pub-9141146483717028/9563044450";
+    public static String ADMOB_INTER_AD_ID = "ca-app-pub-9141146483717028/1132437392";
+
+    public static int SHOW_INTER_ON_CLICKS = 100000;
+    public static boolean isAdsEnabled = true; // REPLACE true TO false IF YOU WANT TO TURN OFF ADS
+
+    public static String ONESIGNAL_APP_ID = "a92srdtfc0-1702-4c4a-b6be-e78b5f9c703c";
+}
